63dc7f63ee39762d5614f7d1d092718d0df7e981
LocalScripte <141340778+LocalScripte@users.noreply.github.com>
2024-04-04 22:22:17 +0300

    Update newtestcontrol.lua

--- newtestcontrol.lua
+++ newtestcontrol.lua
@@ -1,7 +1,7 @@
 local status = false
 repeat
   print("status check!)
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/LocalScripte/test/main/newtestcontrol.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/LocalScripte/test/main/newtestcontrol"))()
    if status == false then
       while true do
           print("Hi")
